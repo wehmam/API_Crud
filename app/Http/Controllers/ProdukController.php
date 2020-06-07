a5ac5a71d5e8536d12db9277f7a5102e0c745a30
wehmam <readytosurff@gmail.com>
2020-06-07 23:54:45 +0700

    update selesai

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\ProdukRequest;
 use App\Produk;
 use \Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ProdukController extends Controller
 {
@@ -31,12 +32,23 @@ class ProdukController extends Controller
     }
 
     public function update(Request $request,$id){
-        $product = Produk::findOrFail($id)->update([
+
+        $product = Produk::find($id);
+        dd($product->foto_produk);
+        $product->update([
             'nama_produk' => $request->nama_produk,
-            'jenis_produk'=> $request->jenis_produk,
-            'harga_produk'=> $request->harga_produk,
-            'foto_produk'=> $request->foto_produk
-        ]); 
+            'jenis_produk' => $request->jenis_produk,
+            'harga_produk' => $request->harga_produk,
+            'foto_produk'=> $request->foto_produk ? $request->foto_produk->hashName() : $product->foto_produk
+        ]);
+        $file = $request->foto_produk;
+        $fotoLocation = 'assets/gambar';
+
+        if($file){
+            Storage::delete('assets/gambar',$product->find($id)->foto_produk);
+            $request->foto_produk->storeAs($fotoLocation,$request->foto_produk->hashName());
+        }
+
         return response()->json([
             'message' => 'Data Berhasil Diedit'
         ]);
@@ -44,11 +56,13 @@ class ProdukController extends Controller
     
     public function show($id){
         $product = Produk::findOrFail($id);
+        // dd($product->foto_produk);
         return response()->json($product);
     }
 
     public function destroy($id){
-        Produk::destroy($id);
+        $product = Produk::destroy($id);
+        Storage::delete('assets/gambar',$product->foto_produk);
         return response()->json([
             'message' => 'Data Berhasil Dihapus'
         ]);
